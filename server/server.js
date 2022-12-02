2bf3e785d62ab79d6bf4c101d26f015011010fdb
Haider <59225711+Ali-raza365@users.noreply.github.com>
2022-12-02 23:15:18 +0500

    update

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ if(process.env.NODE_ENV === 'production'){
 
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 832389328
 app.listen(PORT, () =>{
     console.log('Server is running on port', PORT)
 })
